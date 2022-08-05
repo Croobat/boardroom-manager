4d13ab24d1c29e8fa7cf04ce28aac23c077dd34e
Tony Ramírez <tonyram9906@outlook.com>
2022-08-04 20:48:01 -0500

    testdb added

--- src/components/CrudApp.jsx
+++ src/components/CrudApp.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
+const testDb = [
+  {
+    id: 1,
+    name: "Tony",
+    startTime: new Date(2022, 8, 5, 11, 22),
+    endTime: new Date(2022, 8, 5, 13, 20),
+  },
+  {
+    id: 2,
+    name: "Abby",
+    startTime: new Date(2022, 8, 7, 10, 22),
+    endTime: new Date(2022, 8, 7, 12, 22),
+  },
+  {
+    id: 3,
+    name: "Tania",
+    startTime: new Date(2022, 8, 5, 8, 22),
+    endTime: new Date(2022, 8, 7, 10, 22),
+  }
+]
+
 function CrudApp() {
   return (
     <>
       <h1>CRUD App</h1>
+      <form action=""></form>
+      <table></table>
     </>
   )
 }

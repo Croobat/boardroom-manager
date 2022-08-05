221ada1b9f2079e3510814286f725e593f872577
Tony Ramírez <tonyram9906@outlook.com>
2022-08-04 22:38:26 -0500

    Moved CrudApp to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
-import CrudApp from './components/CrudApp'
+import React, { useState } from 'react'
+import CrudForm from './components/CrudForm'
+import CrudTable from './components/CrudTable'
+
+const testDb = [
+  {
+    id: 1,
+    name: 'Tony',
+    startDate: "2022-08-12",
+    startTime: "11:22",
+    endTime: "13:20"
+  },
+  {
+    id: 2,
+    name: 'Abby',
+    startDate: "2022-08-07",
+    startTime: "10:25",
+    endTime: "12:22"
+  },
+]
 
 function App() {
+  const [db, setDb] = useState(testDb)
+
+  const [dataToEdit, setdataToEdit] = useState(null)
+
+  const createData = (data) => {
+    // Uso la fecha como id único solo por simplicidad
+    data.id = Date.now();
+    setDb([...db, data]);
+   }
+
+  const updateData = (data) => {}
+
+  const deleteData = (id) => {}
+
   return (
-    <div className="App">
-      <h2>Boardroom manager</h2>
-      <CrudApp />
-    </div>
-  );
+    <>
+      <h1>Meeting room manager</h1>
+      <CrudForm
+        createData={createData}
+        updateData={updateData}
+        dataToEdit={dataToEdit}
+        setDataToEdit={setdataToEdit}
+      />
+      <CrudTable data={db} setdataToEdit={setdataToEdit} deleteData={deleteData}/>
+    </>
+  )
 }
 
-export default App;
+export default App
